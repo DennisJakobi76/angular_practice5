c33b784bffcf7734497c2e26c9d4cf225a2fa6c3
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-04-15 10:40:59 +0200

    removed not needed impot from learning-resources-component

--- src/app/learning-resources/learning-resources.component.ts
+++ src/app/learning-resources/learning-resources.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 
 import { SafeLinkDirective } from '../safe-link.directive';
-import { LogDirective } from '../log.directive';
 
 @Component({
   selector: 'app-learning-resources',
   templateUrl: './learning-resources.component.html',
   styleUrl: './learning-resources.component.css',
   standalone: true,
-  imports: [SafeLinkDirective, LogDirective],
+  imports: [SafeLinkDirective],
 })
 export class LearningResourcesComponent {}
